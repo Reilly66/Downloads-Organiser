1733725fd56c895ee630b5e02019a65a5ef0890f
Reilly66 <reillyhowlett@icloud.com>
2022-10-30 01:20:39 +0100

    added misc extension logging

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ EXTENSIONS = {"MICROSOFT_OFFICE": {"doc", "docx", "ppt", "pptx", "xls", "xlsx"},
               "SCRIPTS_AND_EXECUTABLES": {"py", "java", "c", "cs", "cpp", "exe", "js", "json", "html", "css", "jar"}}
 
 DOWNLOADS_DIRECTORY = "C:\\Users\\Reilly\\Downloads"
+UNKNOWN = "UNKNOWN_EXTENSIONS.txt"
 
 
 def main():
@@ -16,13 +17,18 @@ def main():
     for folder_name in EXTENSIONS:
         if folder_name not in folders:
             os.mkdir(os.path.join(DOWNLOADS_DIRECTORY, folder_name))
+    if UNKNOWN not in files:
+        open(os.path.join(DOWNLOADS_DIRECTORY, UNKNOWN), 'x')
     for file_name in files:
-        extension = file_name.split(".")[-1]
-        for folder_name in EXTENSIONS:
-            if extension in EXTENSIONS[folder_name]:
-                os.rename(os.path.join(DOWNLOADS_DIRECTORY, file_name), os.path.join(os.path.join(DOWNLOADS_DIRECTORY, folder_name), file_name))
-            else:
-                os.rename(os.path.join(DOWNLOADS_DIRECTORY, file_name), os.path.join(os.path.join(DOWNLOADS_DIRECTORY, "OTHER"), file_name))
+        if file_name != UNKNOWN:
+            extension = file_name.split(".")[-1]
+            for folder_name in EXTENSIONS:
+                if extension in EXTENSIONS[folder_name]:
+                    os.rename(os.path.join(DOWNLOADS_DIRECTORY, file_name), os.path.join(os.path.join(DOWNLOADS_DIRECTORY, folder_name), file_name))
+                else:
+                    os.rename(os.path.join(DOWNLOADS_DIRECTORY, file_name), os.path.join(os.path.join(DOWNLOADS_DIRECTORY, "OTHER"), file_name))
+                    with open(os.path.join(DOWNLOADS_DIRECTORY, UNKNOWN), "a") as f:
+                        f.write(extension + "\n")
 
 
 if __name__ == "__main__":
